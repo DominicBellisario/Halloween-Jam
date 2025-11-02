0dcf89cf21f650edd3a3da78143edf9da33afa3f
Dominic Bellisario <dlb5102@g.rit.edu>
2025-11-02 13:49:09 -0500

    added new main menu and desk sprite

--- Assets/Scripts/SceneHelper.cs
+++ Assets/Scripts/SceneHelper.cs
@@ -3,23 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneHelper : MonoBehaviour
 {
-    public static SceneHelper Instance { get; private set; }
-
-    void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject); // Only keep the first instance
-            return;
-        }
-
-        Instance = this;
-        DontDestroyOnLoad(gameObject); // Keep across scenes
-    }
-
     // load a scene with the given name
     public void LoadScene(string sceneName)
     {
+        Debug.Log("scene Loaded");
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(sceneName);
     }
